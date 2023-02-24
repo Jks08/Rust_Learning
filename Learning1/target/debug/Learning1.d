3ed9402e6183fb6b520d9086bf89f3ce0119dd4c
Jishnu Kumar Srivastava <jisheras31@gmail.com>
2023-02-24 15:07:40 +0530

    First Thoughts
    Well, this was not fun. The installation was surprisingly very simple.
    But then began the trouble.
    I installed the rust-analyzer extension for VSCode and, it kept giving
    me "rust-analyzer failed to load workspace" error. It still does.
    
    So, I ignored that and did the basic steps: cargo new Learning1 ->
    cd Learning1 -> cargo build -> cargo run. It worked in the terminal.
    I was happy.
    
    Then, I made changes to the main.rs file, did the cargo build and it
    FAILED! :(
    
    That is where I am right now, let's see what this holds in store for me.

---
+++ Learning1/target/debug/Learning1.d
@@ -0,0 +1,1 @@
+/Users/jishnusrivastava/Desktop/rust_learning/Rust_Learning/Learning1/target/debug/Learning1: /Users/jishnusrivastava/Desktop/rust_learning/Rust_Learning/Learning1/src/main.rs
---
+++ Learning1/target/debug/deps/Learning1-b87bf30c61021f93.d
@@ -0,0 +1,5 @@
+/Users/jishnusrivastava/Desktop/rust_learning/Rust_Learning/Learning1/target/debug/deps/Learning1-b87bf30c61021f93: src/main.rs
+
+/Users/jishnusrivastava/Desktop/rust_learning/Rust_Learning/Learning1/target/debug/deps/Learning1-b87bf30c61021f93.d: src/main.rs
+
+src/main.rs:
