857c6a77547ee5735e9d16ef694f8971719a73e6
Jishnu Kumar Srivastava <jisheras31@gmail.com>
2023-02-25 11:40:29 +0530

    gitignore more

---
+++ project1/target/debug/deps/project1-5a21afac563787f0.d
@@ -0,0 +1,5 @@
+/Users/jishnusrivastava/Desktop/Rust_Learning/project1/target/debug/deps/project1-5a21afac563787f0.rmeta: src/main.rs
+
+/Users/jishnusrivastava/Desktop/Rust_Learning/project1/target/debug/deps/project1-5a21afac563787f0.d: src/main.rs
+
+src/main.rs:
---
+++ project1/target/debug/deps/unicode_ident-b228cad35b8f2133.d
@@ -0,0 +1,8 @@
+/Users/jishnusrivastava/Desktop/Rust_Learning/project1/target/debug/deps/unicode_ident-b228cad35b8f2133.rmeta: /Users/jishnusrivastava/.cargo/registry/src/github.com-1ecc6299db9ec823/unicode-ident-1.0.6/src/lib.rs /Users/jishnusrivastava/.cargo/registry/src/github.com-1ecc6299db9ec823/unicode-ident-1.0.6/src/tables.rs
+
+/Users/jishnusrivastava/Desktop/Rust_Learning/project1/target/debug/deps/libunicode_ident-b228cad35b8f2133.rlib: /Users/jishnusrivastava/.cargo/registry/src/github.com-1ecc6299db9ec823/unicode-ident-1.0.6/src/lib.rs /Users/jishnusrivastava/.cargo/registry/src/github.com-1ecc6299db9ec823/unicode-ident-1.0.6/src/tables.rs
+
+/Users/jishnusrivastava/Desktop/Rust_Learning/project1/target/debug/deps/unicode_ident-b228cad35b8f2133.d: /Users/jishnusrivastava/.cargo/registry/src/github.com-1ecc6299db9ec823/unicode-ident-1.0.6/src/lib.rs /Users/jishnusrivastava/.cargo/registry/src/github.com-1ecc6299db9ec823/unicode-ident-1.0.6/src/tables.rs
+
+/Users/jishnusrivastava/.cargo/registry/src/github.com-1ecc6299db9ec823/unicode-ident-1.0.6/src/lib.rs:
+/Users/jishnusrivastava/.cargo/registry/src/github.com-1ecc6299db9ec823/unicode-ident-1.0.6/src/tables.rs:
