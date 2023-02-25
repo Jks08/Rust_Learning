3961228f4b98806b70512974eb6a9b266dc6dd1b
Jishnu Kumar Srivastava <jisheras31@gmail.com>
2023-02-25 11:08:10 +0530

    Addition of 2 numbers

--- project1/src/main.rs
+++ project1/src/main.rs
@@ -1,3 +1,20 @@
+// fn main() {
+//     println!("Hello, world!");
+//     // Github copilot for Rust. 
+// }
+
+// Program to input 2 numbers and print their sum
+
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    let mut num1 = String::new();
+    let mut num2 = String::new();
+    println!("Enter first number");
+    io::stdin().read_line(&mut num1).expect("Failed to read line");
+    println!("Enter second number");
+    io::stdin().read_line(&mut num2).expect("Failed to read line");
+    let num1: i32 = num1.trim().parse().expect("Please type a number!");
+    let num2: i32 = num2.trim().parse().expect("Please type a number!");
+    println!("Sum of {} and {} is {}", num1, num2, num1 + num2);
 }
