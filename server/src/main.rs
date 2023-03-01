19caf7377ce4ef0ac554cfb66ad8969977dcb2ba
Jishnu Kumar Srivastava <jisheras31@gmail.com>
2023-03-01 12:06:06 +0530

    Rust enums

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,4 +1,9 @@
 fn main() {
+    // let get = Method::GET;
+    // let delete = Method::DELETE;
+    // let post = Method::POST;
+    // let put = Method::PUT;
+    
     let server = Server::new("127.0.0.1:8080".to_string());
     server.run();
     // let string = String::from("127.0.0.1:8080");
@@ -31,4 +36,25 @@ impl Server{
     fn run(self){
         println!("Listening on {}", self.addr)
     }
+}
+
+struct Request{
+    // Rust does not support NULL values, but uses enum from Option, from 
+    // standard library. It is a pipe safe way to not encounter no pointer 
+    // exceptions.
+    path: String,
+    query_string: Option<String>,
+    method: String,
+}
+
+enum Method{
+    GET,
+    DELETE,
+    POST,
+    PUT,
+    HEAD,
+    CONNECT,
+    OPTIONS,
+    TRACE,
+    PATCH,
 }
