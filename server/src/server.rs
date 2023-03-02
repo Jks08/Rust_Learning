d579a46bb9a0c66c674b00e049f5842c928762db
Jishnu Kumar Srivastava <jisheras31@gmail.com>
2023-03-02 11:54:12 +0530

    Match expr, Arrays
    to send request to server -> echo "TEST" | netcat 127.0.0.1 8080

--- server/src/server.rs
+++ server/src/server.rs
@@ -1,10 +1,13 @@
 // Struct block
 // If we don't put pub keywrod, then it is private by default.
 use std::net::TcpListener;
+use std::io::Read;
 
 pub struct Server{
     addr: String,
 }
+
+// fn arr(a: &[u8]){}
 
 // Implementation block
 impl Server{
@@ -15,15 +18,44 @@ impl Server{
     }
     // run takes ownership of entire struct. 
     // If we pass in &mut self, it won't take ownership.
-    pub fn run(self) -> (i32, &str, std::net::TcpListener){
+    pub fn run(self){
         println!("Listening on {}", self.addr);
-        let listener = TcpListener::bind(&self.addr).unwrap(); // recoverable error 
+
+        let listener = TcpListener::bind(&self.addr).unwrap(); 
+        // recoverable error 
 
         // while true{} // Normal infinite loop, but Rust has the following special:
         loop{
-            listener.accept();
-        }
+            match listener.accept(){ // Need to specify Ok and Err
+                // Ok((stream, _)) => { don't care about the second value }
+                // Ok((stream, addr)) => { addr is a tuple } 
+                Ok((mut stream, _)) => {
+                    let mut buffer = [0;1024]; // 0 is initial value, 1024 is size
+                    match stream.read(&mut buffer){
+                        Ok(_) => {
+                            println!("Received Request: {}", String::from_utf8_lossy(&buffer));
+                        }
+                        Err(e) => {
+                            println!("Failed to read from connection: {}", e);
+                        }
+                    }
+                    // sample array : 
+                    // let a = [1,2,3,4,5]; // type is [i32;5]
+                    // arr(&a);
+                }
+                Err(e) => {
+                    println!("Connection failed: {}", e);
+                }
+            }
+            // let res = listener.accept();
+            // if res.is_err(){
+            //     continue;
+            // }
+
+            // let (stream,addr) = res.unwrap();
+        } // Not the best way to handle enum errors. Match is better.
+
         // let tup = (5,"a",listener);
-        (5,"a",listener)
+        // (5,"a",listener)
     }
 }
