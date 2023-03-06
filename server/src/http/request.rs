f6b8b61769e728ed533c7558c607b2161cc65b6a
Jishnu Kumar Srivastava <jisheras31@gmail.com>
2023-03-06 12:20:40 +0530

    Converting option to result

--- server/src/http/request.rs
+++ server/src/http/request.rs
@@ -31,6 +31,19 @@ impl TryFrom<&[u8]> for Request{
         // }
 
         let request = str::from_utf8(buf)?;
+        // match get_next_word(request){
+        //     Some((method, request)) => {},
+        //     None => return Err(ParseError::InvalidRequest),
+
+        // }
+
+        let (method, request) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
+        let (path, request) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
+        let (protocol, _) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
+
+        if protocol != "HTTP/1.1"{
+            return Err(ParseError::InvalidProtocol);
+        }
         // ? will try to convert the result to type.
         unimplemented!()
     }
