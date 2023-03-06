e00dfd7ae32c867772245bce29df0b8f24b34e3a
Jishnu Kumar Srivastava <jisheras31@gmail.com>
2023-03-06 11:45:00 +0530

    Advanced Error Handling

--- server/src/http/request.rs
+++ server/src/http/request.rs
@@ -2,6 +2,8 @@ use super::method::Method;
 use std::convert::TryFrom;
 use std::error::Error;
 use std::fmt::{Debug, Display, Formatter, Result as FmtResult};
+use std::str;
+use std::str::Utf8Error;
 
 pub struct Request{
     // Rust does not support NULL values, but uses enum from Option, from 
@@ -18,6 +20,18 @@ impl TryFrom<&[u8]> for Request{
     type Error = ParseError;
 
     fn try_from(buf: &[u8]) -> Result<Self, Self::Error> {
+        // match str::from_utf8(buf){
+        //     Ok(request) => {},
+        //     Err(_) => return Err(ParseError::InvalidEncoding), 
+        // }
+
+        // match str::from_utf8(buf).or(Err(ParseError::InvalidEncoding)){
+        //     Ok(request) => {},
+        //     Err(e) => return Err(e),
+        // }
+
+        let request = str::from_utf8(buf)?;
+        // ? will try to convert the result to type.
         unimplemented!()
     }
 }
@@ -37,6 +51,12 @@ impl ParseError{
             Self::InvalidProtocol => "Invalid Protocol",
             Self::InvalidMethod => "Invalid Method",
         }
+    }
+}
+
+impl From<Utf8Error> for ParseError{
+    fn from(_: Utf8Error) -> Self {
+        Self::InvalidEncoding
     }
 }
 
