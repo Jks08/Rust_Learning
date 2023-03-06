3688ffec1ae63527b8867fa4ccc9cd7ce48d0c06
Jishnu Kumar Srivastava <jisheras31@gmail.com>
2023-03-06 12:47:04 +0530

    If-let statement

--- server/src/http/request.rs
+++ server/src/http/request.rs
@@ -38,7 +38,7 @@ impl TryFrom<&[u8]> for Request{
         // }
 
         let (method, request) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
-        let (path, request) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
+        let (mut path, request) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
         let (protocol, _) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
 
         if protocol != "HTTP/1.1"{
@@ -46,6 +46,26 @@ impl TryFrom<&[u8]> for Request{
         }
 
         let method: Method = method.parse()?;
+
+        let mut query_string = None;
+        // match path.find('?'){
+        //     Some(i) => {
+        //         query_string = Some(path[i+1..].to_string());
+        //         path = &path[..i];
+        //     }
+        //     None => {}
+        // }
+        // let q = path.find('?');
+        // if q.is_some(){
+        //     let i = q.unwrap();
+        //     query_string = Some(&path[i+1..]);
+        //     path = &path[..i];
+        // }
+
+        if let Some(i) = path.find('?'){
+            query_string = Some(path[i+1..].to_string());
+            path = &path[..i];
+        }
         // ? will try to convert the result to type.
         unimplemented!()
     }
